1f0ca562a772628e73d5dc0822bb5f8eee80539b
Artem Venger <venger@tiltshift.xyz>
2024-08-16 07:28:52 +0700

    Created coroutine manager

--- Assets/Project/Code/Rewards.Unity/Game/EntryPoint.cs
+++ Assets/Project/Code/Rewards.Unity/Game/EntryPoint.cs
@@ -1,6 +1,8 @@
+using Rewards.Coroutine;
 using Rewards.LootBox.Model;
 using Rewards.Storage.SaveStrategy;
 using Rewards.Storage.SaveStrategy.FileOperations;
+using Rewards.Unity.Coroutine.Manager;
 using Rewards.Unity.LootBox.Config.SO;
 using UnityEngine;
 
@@ -14,12 +16,17 @@ namespace Rewards.Unity.Game
         private string _saveDataPath;
         private IFileOperations _fileOperations;
         private ISaveStrategy _saveStrategy;
+        private ICoroutineManager _coroutineManager;
 
         private void Awake()
         {
             _saveDataPath = Application.persistentDataPath;
             _fileOperations = new FileOperations();
             _saveStrategy = new JsonFileStrategy(_fileOperations, _saveDataPath);
+
+            var coroutineManagerGO = new GameObject(nameof(ICoroutineManager));
+            DontDestroyOnLoad(coroutineManagerGO);
+            _coroutineManager = coroutineManagerGO.AddComponent<CoroutineManager>();
 
             foreach (var currentConfig in _lootBoxCollectionConfigSO.Boxes)
             {
