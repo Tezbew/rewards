aad3d10e4014a09c4268a9b57aeeb6fba7a829de
Artem Venger <venger@tiltshift.xyz>
2024-08-28 17:23:11 +0700

    Changed serialization attribute

--- Assets/Project/Code/Rewards/Storage/Profile/Data/ProfileData.cs
+++ Assets/Project/Code/Rewards/Storage/Profile/Data/ProfileData.cs
@@ -1,11 +1,11 @@
+using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 using Rewards.Item;
 using Rewards.Resource;
 
 namespace Rewards.Storage.Profile.Data
 {
-    [JsonObject]
+    [Serializable]
     public class ProfileData
     {
         public Dictionary<ResourceType, int> Resources = new();
